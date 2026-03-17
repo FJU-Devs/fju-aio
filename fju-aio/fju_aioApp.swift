ae0174edd54883047fc5e43eba6d89837e531439
nelsonGX <hi@nelsongx.com>
2026-03-17 15:49:58 +0800

    Initial Commit

---
+++ fju-aio/fju_aioApp.swift
@@ -0,0 +1,17 @@
+//
+//  fju_aioApp.swift
+//  fju-aio
+//
+//  Created by Nelson Lin on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct fju_aioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
